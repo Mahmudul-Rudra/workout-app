18619d175dd4ed17e90f80c83884be87ee271867
S.M. Mahmudul Hasan Rudra <71984328+Mahmudul-Rudra@users.noreply.github.com>
2024-03-13 12:18:53 +0600

    database added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 // we will register the express app here. It is the entry file for the backend.
+require("dotenv").config();
 
 const express = require("express");
+const workoutRoutes = require("./routes/workouts");
+
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -8,7 +11,25 @@ const mongoose = require("mongoose");
 // express app
 const app = express();
 
-//listen on http://localhost
-app.listen(8080, () => {
-  console.log("Server is running on port 8080......");
+// middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
 });
+
+// reacting to the request-routes
+app.use("/api/workouts", workoutRoutes);
+
+// connect to database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen on http://localhost
+    app.listen(process.env.PORT, () => {
+      console.log("Server is running on port " + process.env.PORT + "...");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
