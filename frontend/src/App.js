09304804f5301606016f63716bdf69a2d712f380
S.M. Mahmudul Hasan Rudra <71984328+Mahmudul-Rudra@users.noreply.github.com>
2024-03-15 00:36:29 +0600

    frontend app created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// pages and components
+import Home from "./pages/Home";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
